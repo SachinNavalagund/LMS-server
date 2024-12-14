3097bc57651b1cf504b54b3127815bbfb9b348b1
SachinNavalagund <sachinnavalagund1@gmail.com>
2024-12-14 11:40:06 +0530

    user clerk api with middleware

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,26 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import * as dynamoose from "dynamoose";
+import {
+  clerkMiddleware,
+  createClerkClient,
+  requireAuth,
+} from "@clerk/express";
 // ROUTE IMPORTS
 import courseRoutes from "./routes/courseRoutes";
+import userClerkRoutes from "./routes/userClerkRoutes";
 
 // CONFIGURATIONS
 dotenv.config();
 
 const isProduction = process.env.NODE_ENV === "production";
-
 if (!isProduction) {
   dynamoose.aws.ddb.local();
 }
+
+export const clerkClient = createClerkClient({
+  secretKey: process.env.CLERK_SECRET_KEY,
+});
 
 // MIDDLEWARES
 const app = express();
@@ -26,6 +35,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+app.use(clerkMiddleware());
 
 // ROUTES
 app.get("/", (req, res) => {
@@ -33,6 +43,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/courses", courseRoutes);
+app.use("/users/clerk", requireAuth(), userClerkRoutes);
 
 // SERVER
 const port = process.env.PORT || 3000;
