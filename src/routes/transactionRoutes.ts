457c3098bb3af9c16e5625a278f2953fbbac233a
SachinNavalagund <sachinnavalagund1@gmail.com>
2024-12-15 08:38:51 +0530

    transaction api

---
+++ src/routes/transactionRoutes.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+import {
+  createStripePaymentIntent,
+  createTransaction,
+} from "../controllers/transactionController";
+
+const router = express.Router();
+
+router.post("/", createTransaction);
+router.post("/stripe/payment-intent", createStripePaymentIntent);
+
+export default router;
